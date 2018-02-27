34a66bd734011163c6af8dc370c036734781e2c8
DC <danny_chauhan@hotmail.com>
2018-02-27 14:35:46 +0000

    added  security groups needed to connect to exsternal wordpress EC2 machine

---
+++ security_groups.tf
@@ -0,0 +1,38 @@
+/* provider "aws" {
+    access_key  = "ACCESS_KEY_HERE"
+    secret_key = "SECRET_KEY_HERE"
+    region     = "us-west-2" 
+} */
+
+#defines the new resource being made in this case this is the AWS security group
+resource "aws_security_group" "rules" {
+  name        = "Wordpress Security Group Rules"
+  description = "Allows inbound and outbound traffic"
+}
+
+variable "http_ports" {
+  default = ["80", "443", "3306"]
+}
+
+#defines inbound rules set by the http_ports variable
+resource "aws_security_group_rule" "ingress_http" {
+  count = "${length(var.http_ports)}"
+
+  type        = "ingress"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = "${element(var.http_ports, count.index)}"
+  to_port     = "${element(var.http_ports, count.index)}"
+
+  security_group_id = "${aws_security_group.rules.id}"
+}
+
+#defines outbound rules stating that all ports will be open on the outbound
+resource "aws_security_group_rule" "egress_rules" {
+    type = "egress"
+    protocol ="-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    security_group_id = "${aws_security_group.rules.id}"
+}
